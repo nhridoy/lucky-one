49f334d124ebacd63f85cfd9549e5143c359d015
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-25 23:41:45 +0600

    Body and inner component intieted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Head from "./components/Head/Head";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
     <div className="container mx-auto p-5">
       <Head />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+
+const Body = () => {
+  return (
+    <div>
+      <Product />
+      <Cart />
+    </div>
+  );
+};
+
+export default Body;
