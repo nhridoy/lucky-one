cb96cfc9b16b6aad3ef1a22ec90706ca31eb697d
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-26 00:02:30 +0600

    Product Data Loaded

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Product = () => {
-  return <div>Product</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    // Load Computer Products from API
+    fetch("data.json")
+      // Convert response to JSON
+      .then((response) => response.json())
+      // Set products state to JSON data
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="font-bold text-xl">Products</h2>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+        {products.map((product) => (
+          <div key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Product;
